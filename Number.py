b21103a9d034842c0299f1188c0c6af6012ea94c
Feely <feelysong@gmail.com>
2014-05-30 17:12:09 +0800

    v1.0

--- Number.py
+++ Number.py
@@ -115,31 +115,31 @@ def main():
     p_cq=multiprocessing.Process(name='CQSSC',target=ssc_drawnumber,args=(ssc_type,db_ssc_type,))
     p_cq.start()
     p_cq.join(timeout=10)
-    #天津时时彩
-    db_ssc_type='TSC'
-    p_tj=multiprocessing.Process(name='TJSSC',target=tjssc_drawnumber,args=(db_ssc_type,))
-    p_tj.start()
-    p_tj.join(10)
-
-    #江西时时彩
-    ssc_type='jxssc'
-    db_ssc_type='XSC'
-    p_jx=multiprocessing.Process(name='JXSSC',target=jxssc_drawnumber,args=(ssc_type,db_ssc_type,))
-    p_jx.start()
-    p_jx.join(10)
-
-    #广东十分彩
-    gdsf_db_type='GDS'
-    p_gdsf=multiprocessing.Process(name='GDSFC',target=gdsf_drawnumber,args=(gdsf_db_type,))
-    p_gdsf.start()
-    p_gdsf.join(timeout=10)
-
-    #广东11选5
-    ssc_type='gd11x5'
-    db_11x5_type='SYX'
-    p_11x5=multiprocessing.Process(name='GD11X5',target=gd11x5,args=(ssc_type,db_11x5_type,))
-    p_11x5.start()
-    p_11x5.join(10)
+    # #天津时时彩
+    # db_ssc_type='TSC'
+    # p_tj=multiprocessing.Process(name='TJSSC',target=tjssc_drawnumber,args=(db_ssc_type,))
+    # p_tj.start()
+    # p_tj.join(10)
+    #
+    # #江西时时彩
+    # ssc_type='jxssc'
+    # db_ssc_type='XSC'
+    # p_jx=multiprocessing.Process(name='JXSSC',target=jxssc_drawnumber,args=(ssc_type,db_ssc_type,))
+    # p_jx.start()
+    # p_jx.join(10)
+    #
+    # #广东十分彩
+    # gdsf_db_type='GDS'
+    # p_gdsf=multiprocessing.Process(name='GDSFC',target=gdsf_drawnumber,args=(gdsf_db_type,))
+    # p_gdsf.start()
+    # p_gdsf.join(timeout=10)
+    #
+    # #广东11选5
+    # ssc_type='gd11x5'
+    # db_11x5_type='SYX'
+    # p_11x5=multiprocessing.Process(name='GD11X5',target=gd11x5,args=(ssc_type,db_11x5_type,))
+    # p_11x5.start()
+    # p_11x5.join(10)
 
 if __name__ == "__main__":
     main()
